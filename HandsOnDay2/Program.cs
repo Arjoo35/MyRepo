fce2182be8085fc8a5ebad6a1b267e900dce18e0
Arjoo Shaikh <arjooshaikh35@gmail.com>
2021-08-18 16:27:15 +0530

    initial

---
+++ HandsOnDay2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HandsOnDay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            if (a > 0)
+            {
+                Console.WriteLine("A is positive");
+            }
+            else
+                Console.WriteLine("A is negative");
+            int b = 13;
+            if (b % 2 == 0)
+            {
+                Console.WriteLine("Num is even");
+            }
+            else
+                Console.WriteLine("Num is odd");
+        }
+    }
+}
---
+++ HandsOnDay2/nested_if.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnDay2
+{
+    class nested_if
+    {
+    }
+}
